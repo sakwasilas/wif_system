25b358806a0463e183bb63eeaf6f83a22a4545c7
sakwasilas <sakwahsilas@gmail.com>
2025-10-09 11:00:36 +0300

    changed database format

--- connections.py
+++ connections.py
@@ -2,11 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 
 # ✅ New database URL
-DATABASE_URL = "postgresql://wifi_system_zpes_user:YR5eOiEeYHWC3ZqEPWuMSWhDU8mP5nr7@dpg-d3jk97p5pdvs73egi8dg-a.oregon-postgres.render.com:5432/wifi_system_zpes?sslmode=require"
+DATABASE_URL = "postgresql://wifi_4z1b_user:ttgov76KC9Juh3be3GlDyKGCt3cvCmEI@dpg-d3jmlk9r0fns738cjhe0-a.oregon-postgres.render.com:5432/wifi_4z1b?sslmode=require"
 
+# Create SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
 
+# Create session factory
 Session = scoped_session(sessionmaker(bind=engine))
 SessionLocal = Session
 
+# Declare base class for models
 Base = declarative_base()
